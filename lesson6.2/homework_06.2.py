deead57952b5274623be313bd553a5051bd40e95
PitonWuk <rzavadskyi777@gmail.com>
2024-09-06 00:18:30 +0300

    added homework_06.3

--- lesson6.2/homework_06.2.py
+++ lesson6.2/homework_06.2.py
@@ -1,14 +1,14 @@
-# # Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є літера "h"
-# # (враховуються як великі так і маленькі). Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
-#
-# while True:
-#     word_with_h = input("Enter the word with 'h/H' character: ").lower()
-#
-#     if 'h' in word_with_h:
-#         print("Done. Very good!")
-#         break
-#     else:
-#         print("There is no 'h'. Please, enter the word with 'h' or 'H' characters: ")
+# Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є літера "h"
+# (враховуються як великі так і маленькі). Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
+
+while True:
+    word_with_h = input("Enter the word with 'h/H' character: ").lower()
+
+    if 'h' in word_with_h:
+        print("Done. Very good!")
+        break
+    else:
+        print("There is no 'h'. Please, enter the word with 'h' or 'H' characters: ")
 
 
 
