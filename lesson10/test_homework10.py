c5cd8f7209b0c88674218f7bba7ebf1573c3f029
PitonWuk <rzavadskyi777@gmail.com>
2024-10-20 18:38:08 +0300

    added file homework3

---
+++ lesson10/test_homework10.py
@@ -0,0 +1,32 @@
+import pytest
+import logging
+from homework10 import log_event
+
+
+def test_log_event_success(caplog):
+    with caplog.at_level(logging.INFO):
+        log_event('user1', 'success')
+    assert caplog.records[0].message == 'Login event - Username: user1, Status: success'
+    assert caplog.records[0].levelname == 'INFO'
+
+
+def test_log_event_expired(caplog):
+    with caplog.at_level(logging.WARNING):
+        log_event('user2', 'expired')
+    assert caplog.records[0].message == 'Login event - Username: user2, Status: expired'
+    assert caplog.records[0].levelname == 'WARNING'
+
+
+def test_log_event_failed(caplog):
+    with caplog.at_level(logging.ERROR):
+        log_event('user3', 'failed')
+    assert caplog.records[0].message == 'Login event - Username: user3, Status: failed'
+    assert caplog.records[0].levelname == 'ERROR'
+
+
+def test_log_event_unknown_status(caplog):
+    with caplog.at_level(logging.ERROR):
+        log_event('user4', 'unknown')
+    assert caplog.records[0].message == 'Login event - Username: user4, Status: unknown'
+    assert caplog.records[0].levelname == 'ERROR'
+
